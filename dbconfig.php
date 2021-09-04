8a4efbf7847ca445e09579cb419160147b2d0379
vk-0002 <vaibhavkshirsagar225@gmail.com>
2021-09-04 12:45:33 +0530

    initial commit

---
+++ dbconfig.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_USER', "id11775798_vaibhav");     // Your database user name
+define('DB_PASSWORD', "0987654321");			// Your database password (mention your db password here)
+define('DB_DATABASE', "id11775798_vaibhav"); // Your database name
+define('DB_SERVER', "localhost");			// db server (Mostly will be 'local' host)
+
+?>
